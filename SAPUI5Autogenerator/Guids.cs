cc334edf7b0f22109497cbf50b84e085054cc97a
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-17 04:17:41 -0400

    Added Plugin Project

---
+++ SAPUI5Autogenerator/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace AtSebak.SAPUI5Autogenerator
+{
+    static class GuidList
+    {
+        public const string guidSAPUI5AutogeneratorPkgString = "b8fd1f0c-040d-4f3c-b28e-75b39cbc310b";
+        public const string guidSAPUI5AutogeneratorCmdSetString = "ebda2301-f598-4ac7-828e-2b9a4ea256bc";
+
+        public static readonly Guid guidSAPUI5AutogeneratorCmdSet = new Guid(guidSAPUI5AutogeneratorCmdSetString);
+    };
+}
