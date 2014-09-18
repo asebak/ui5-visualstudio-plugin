37f6a63aadae4e80d11761d46b9b5203076fe274
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-18 05:51:40 -0400

    update wizard provider added dll reference to project template wizard

---
+++ SapUI5AppWizard/SapUI5AppWizard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SapUI5AppWizard
+{
+    public partial class SapUI5AppWizard : Form
+    {
+        public SapUI5AppWizard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SapUI5AppWizard/SapUI5WizardProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace SapUI5AppWizard
+{
+    public class SapUI5WizardProvider: IWizard
+    {
+        private SapUI5AppWizard mForm;
+        private DTE mDTE;
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            this.mForm = new SapUI5AppWizard();
+            this.mForm.ShowDialog();
+            if (this.mForm.DialogResult == DialogResult.Cancel)
+            {
+                var destinationDirectory = replacementsDictionary["$destinationdirectory$"];
+                if (Directory.Exists(destinationDirectory))
+                {
+                    Directory.Delete(destinationDirectory, true);
+                }
+                throw new WizardBackoutException();
+            }
+            this.mDTE = automationObject as DTE;
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunFinished()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
