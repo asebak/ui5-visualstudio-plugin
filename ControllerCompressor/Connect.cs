4de23b123b5093696cd8bd9e998d9c79a5dfcaad
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-27 13:13:21 -0400

    fix add on not working

--- ControllerCompressor/Connect.cs
+++ ControllerCompressor/Connect.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using Extensibility;
+﻿#region Using
+
+using System;
 using EnvDTE;
 using EnvDTE80;
+using Extensibility;
 using Microsoft.VisualStudio.CommandBars;
-using System.Resources;
-using System.Reflection;
-using System.Globalization;
 
-namespace ViewControllerCompressor
+#endregion
+
+namespace ControllerCompressor
 {
-	/// <summary>The object for implementing an Add-in.</summary>
-	/// <seealso class='IDTExtensibility2' />
+    /// <summary>The object for implementing an Add-in.</summary>
+    /// <seealso class='IDTExtensibility2' />
     public class Connect : IDTExtensibility2, IDTCommandTarget
     {
-        /// <summary>Implements the constructor for the Add-in object. Place your initialization code within this method.</summary>
-        public Connect()
-        {
-        }
-
         /// <summary>
-        /// Implements the OnConnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is being loaded.
+        ///     Implements the OnConnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is
+        ///     being loaded.
         /// </summary>
         /// <param name="application">The application.</param>
         /// <param name="connectMode">The connect mode.</param>
@@ -28,21 +25,28 @@ namespace ViewControllerCompressor
         /// <seealso class="IDTExtensibility2" />
         public void OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
         {
-            _applicationObject = (DTE2)application;
-            _addInInstance = (AddIn)addInInst;
+            this._applicationObject = (DTE2) application;
+            this._addInInstance = (AddIn) addInInst;
             if (connectMode == ext_ConnectMode.ext_cm_UISetup)
             {
-                var contextGUIDS = new object[] { };
-                var commands = (Commands2)_applicationObject.Commands;
-                var cmdBars = (CommandBars)(_applicationObject.CommandBars);
-                var vsBarProject = cmdBars["Folder"];
+                var contextGUIDS = new object[] {};
+                var commands = (Commands2)this._applicationObject.Commands;
+                const string toolsMenuName = "Tools";
+                var menuBarCommandBar = ((CommandBars)this._applicationObject.CommandBars)["MenuBar"];
+                var toolsControl = menuBarCommandBar.Controls[toolsMenuName];
+                var cmdBars = (CommandBars) (_applicationObject.CommandBars);
+                var vsBarProject = cmdBars["Project"];
+                var toolsPopup = (CommandBarPopup) toolsControl;
                 CommandBarButton control = null;
                 try
                 {
-                    var command = commands.AddNamedCommand2(_addInInstance, "Minimizer", "Minimizer", "Executes the command for Minimizer", true, 2817, ref contextGUIDS);
+                    var command = commands.AddNamedCommand2(_addInInstance, "ControllerCompressor",
+                        "UI5 View/Controller Minimizer", "Executes the command for ControllerCompressor", true, 2817,
+                        ref contextGUIDS);
+
                     if ((command != null) && (vsBarProject != null))
                     {
-                        control = (CommandBarButton)command.AddControl(vsBarProject);
+                        control = (CommandBarButton) command.AddControl(vsBarProject);
                     }
                 }
                 catch (ArgumentException)
@@ -56,7 +60,7 @@ namespace ViewControllerCompressor
         }
 
         /// <summary>
-        /// Minimizes the control_ click.
+        ///     Minimizes the control_ click.
         /// </summary>
         /// <param name="ctrl">The control.</param>
         /// <param name="canceldefault">if set to <c>true</c> [canceldefault].</param>
@@ -67,7 +71,8 @@ namespace ViewControllerCompressor
         }
 
         /// <summary>
-        /// Implements the OnDisconnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is being unloaded.
+        ///     Implements the OnDisconnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is
+        ///     being unloaded.
         /// </summary>
         /// <param name="disconnectMode">The disconnect mode.</param>
         /// <param name="custom">The custom.</param>
@@ -77,7 +82,8 @@ namespace ViewControllerCompressor
         }
 
         /// <summary>
-        /// Implements the OnAddInsUpdate method of the IDTExtensibility2 interface. Receives notification when the collection of Add-ins has changed.
+        ///     Implements the OnAddInsUpdate method of the IDTExtensibility2 interface. Receives notification when the collection
+        ///     of Add-ins has changed.
         /// </summary>
         /// <param name="custom">The custom.</param>
         /// <seealso class="IDTExtensibility2" />
@@ -86,7 +92,8 @@ namespace ViewControllerCompressor
         }
 
         /// <summary>
-        /// Implements the OnStartupComplete method of the IDTExtensibility2 interface. Receives notification that the host application has completed loading.
+        ///     Implements the OnStartupComplete method of the IDTExtensibility2 interface. Receives notification that the host
+        ///     application has completed loading.
         /// </summary>
         /// <param name="custom">The custom.</param>
         /// <seealso class="IDTExtensibility2" />
@@ -95,7 +102,8 @@ namespace ViewControllerCompressor
         }
 
         /// <summary>
-        /// Implements the OnBeginShutdown method of the IDTExtensibility2 interface. Receives notification that the host application is being unloaded.
+        ///     Implements the OnBeginShutdown method of the IDTExtensibility2 interface. Receives notification that the host
+        ///     application is being unloaded.
         /// </summary>
         /// <param name="custom">The custom.</param>
         /// <seealso class="IDTExtensibility2" />
@@ -104,27 +112,28 @@ namespace ViewControllerCompressor
         }
 
         /// <summary>
-        /// Implements the QueryStatus method of the IDTCommandTarget interface. This is called when the command's availability is updated
+        ///     Implements the QueryStatus method of the IDTCommandTarget interface. This is called when the command's availability
+        ///     is updated
         /// </summary>
         /// <param name="commandName">Name of the command.</param>
         /// <param name="neededText">The needed text.</param>
         /// <param name="status">The status.</param>
         /// <param name="commandText">The command text.</param>
         /// <seealso class="Exec" />
-        public void QueryStatus(string commandName, vsCommandStatusTextWanted neededText, ref vsCommandStatus status, ref object commandText)
+        public void QueryStatus(string commandName, vsCommandStatusTextWanted neededText, ref vsCommandStatus status,
+            ref object commandText)
         {
             if (neededText == vsCommandStatusTextWanted.vsCommandStatusTextWantedNone)
             {
-                if (commandName == "Minimizer.Connect.Minimizer")
+                if (commandName == "ControllerCompressor.Connect.ControllerCompressor")
                 {
-                    status = (vsCommandStatus)vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled;
-                    return;
+                    status = vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled;
                 }
             }
         }
 
         /// <summary>
-        /// Implements the Exec method of the IDTCommandTarget interface. This is called when the command is invoked.
+        ///     Implements the Exec method of the IDTCommandTarget interface. This is called when the command is invoked.
         /// </summary>
         /// <param name="commandName">Name of the command.</param>
         /// <param name="executeOption">The execute option.</param>
@@ -132,18 +141,19 @@ namespace ViewControllerCompressor
         /// <param name="varOut">The variable out.</param>
         /// <param name="handled">if set to <c>true</c> [handled].</param>
         /// <seealso class="Exec" />
-        public void Exec(string commandName, vsCommandExecOption executeOption, ref object varIn, ref object varOut, ref bool handled)
+        public void Exec(string commandName, vsCommandExecOption executeOption, ref object varIn, ref object varOut,
+            ref bool handled)
         {
             handled = false;
             if (executeOption == vsCommandExecOption.vsCommandExecOptionDoDefault)
             {
-                if (commandName == "Minimizer.Connect.Minimizer")
+                if (commandName == "ControllerCompressor.Connect.ControllerCompressor")
                 {
                     handled = true;
-                    return;
                 }
             }
         }
+
         private DTE2 _applicationObject;
         private AddIn _addInInstance;
     }
